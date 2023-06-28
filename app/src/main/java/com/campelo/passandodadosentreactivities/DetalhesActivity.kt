c41a7c431a36ee9cbfc0717fce1266981c2244c7
José Campelo <josecampelojr@gmail.com>
2023-06-28 18:00:34 -0300

    Finalizando o projeto, adicionando a lógica de detalhes para receber os dados da intent passada pela main

--- app/src/main/java/com/campelo/passandodadosentreactivities/DetalhesActivity.kt
+++ app/src/main/java/com/campelo/passandodadosentreactivities/DetalhesActivity.kt
@@ -2,10 +2,35 @@ package com.campelo.passandodadosentreactivities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class DetalhesActivity : AppCompatActivity() {
+
+    private lateinit var txtNome: TextView
+    private lateinit var txtPeso: TextView
+    private lateinit var txtAltura: TextView
+    private lateinit var btnVoltar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
+
+        txtNome = findViewById(R.id.txtNome)
+        txtPeso = findViewById(R.id.txtPeso)
+        txtAltura = findViewById(R.id.txtAltura)
+        btnVoltar = findViewById(R.id.btnVoltar)
+
+        val bundle = intent.extras
+
+        if(bundle != null){
+            txtNome.text = "Nome: ${bundle.getString("nome")}"
+            txtPeso.text = "Peso: ${bundle.getString("peso")}"
+            txtAltura.text = "Altura: ${bundle.getString("altura")}"
+        }
+
+        btnVoltar.setOnClickListener {
+            finish()
+        }
     }
 }
