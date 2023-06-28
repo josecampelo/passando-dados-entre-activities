2ae3830ab8ff1d67728328048b94b2f9b9b5b8e1
José Campelo <josecampelojr@gmail.com>
2023-06-28 17:46:28 -0300

    Criação da lógica para armazenar os dados e transferir para a detalhes activity

--- app/src/main/java/com/campelo/passandodadosentreactivities/MainActivity.kt
+++ app/src/main/java/com/campelo/passandodadosentreactivities/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.campelo.passandodadosentreactivities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edtNome: EditText
+    private lateinit var edtPeso: EditText
+    private lateinit var edtAltura: EditText
+    private lateinit var btnCadastrar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtNome = findViewById(R.id.edtNome)
+        edtPeso = findViewById(R.id.edtPeso)
+        edtAltura = findViewById(R.id.edtAltura)
+        btnCadastrar = findViewById(R.id.btnCadastrar)
+
+        btnCadastrar.setOnClickListener {
+            val intent = Intent(this, DetalhesActivity::class.java)
+
+            intent.putExtra("nome", edtNome.text.toString())
+            intent.putExtra("peso", edtPeso.text.toString())
+            intent.putExtra("altura", edtAltura.text.toString())
+
+            startActivity(intent)
+        }
     }
 }
